fbc44d702cd5f12de2d52a5acc04dfecf53b08b7
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2023-05-28 15:33:20 +0600

    portfolio created

---
+++ src/mock/profile.js
@@ -0,0 +1,19 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://github.com/Rasel105',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://www.linkedin.com/in/azsrasel/',
+    icon: 'akar-icons:linkedin-fill',
+  },
+];
+
+export const HERO_TITLES = ['Azadul Islam Rasel', 'Full-stack Developer', 'Javascript Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack Developer with experience in designing, developing and maintaining web applications using technologies such as JavaScript, React, Node.js. ';
+
+export const contactEmail = 'azsrasel.dev@gmail.com';
+
+export const documentTitle = 'Hello I am Azadul Islam Rasel | Full Stack Developer | Javascript Enthusiast';
